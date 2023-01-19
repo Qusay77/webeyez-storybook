d338ae2d25867bd8ae2dd411ac258b13842179a0
Qusay <qusay@nadsoft.net>
2023-01-19 09:14:59 +0300

    init

---
+++ stories/Button.stories.tsx
@@ -0,0 +1,34 @@
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+import { CoolButton } from "packages/button";
+export default {
+	title: "Example/Button",
+	component: CoolButton,
+	argTypes: {
+		backgroundColor: { control: "color" },
+		handleClick: {
+			action: "clicked",
+		},
+	},
+} as ComponentMeta<typeof CoolButton>;
+
+const Template: ComponentStory<typeof CoolButton> = (args) => (
+	<CoolButton {...args} />
+);
+
+export const Large = Template.bind({});
+Large.args = {
+	size: "large",
+	label: "large",
+};
+
+export const Medium = Template.bind({});
+Medium.args = {
+	size: "medium",
+	label: "med",
+};
+
+export const Small = Template.bind({});
+Small.args = {
+	size: "small",
+	label: "small",
+};
